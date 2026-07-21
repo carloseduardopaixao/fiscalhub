dbad7576e23e868e774fc772fb9ad84e9b5aa00f
Carlos Eduardo <118945430+C4rlosPS@users.noreply.github.com>
2026-07-21 20:02:08 -0300

    inicializa projeto Spring Boot

---
+++ src/main/java/io/github/carloseduardopaixao/fiscalhub/FiscalhubApplication.java
@@ -0,0 +1,13 @@
+package io.github.carloseduardopaixao.fiscalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/carloseduardopaixao/fiscalhub/FiscalhubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carloseduardopaixao.fiscalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
